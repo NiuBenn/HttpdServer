17eff617d43ba37bdead0111c5fb55a8fe75846f
NiuBenn <1402435102@qq.com>
2019-01-16 15:54:54 +0800

    add signal(SIGPIPE, SIG_IGN)

--- HttpdServer.cc
+++ HttpdServer.cc
@@ -1,6 +1,6 @@
 #include"HttpdServer.hpp"
 #include<unistd.h>
-
+#include<signal.h>
 void Usage(std::string procname)
 {
 	std::cout<< "How to use: " << procname << " port" << std::endl;
@@ -13,7 +13,9 @@ int main(int argc, char *argv[])
 		Usage(argv[0]);	//打印使用手册
 		exit(1);
 	}
-	
+
+    signal(SIGPIPE, SIG_IGN);
+
 	HttpdServer* server = new HttpdServer(atoi(argv[1]));	//new HttpdServer对象
 	server->Init();		//对象初始化
 	server->Start();	//启动server
